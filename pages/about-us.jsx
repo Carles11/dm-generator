f1513b7f202d69b3c02ccfdef051e6b84875993e
carlos <carlos@3pace.dev>
2023-02-17 18:28:12 -0300

    aboutUs structured

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -1,9 +1,40 @@
 import Layout from 'components/layout'
+import styles from 'styles/AboutUs.module.scss'
 
 export default function aboutUs() {
   return (
     <Layout>
-      <h1>Who we are</h1>
+      <div className={styles.aboutUsContainer}>
+        <div className={styles.aboutUsContainer_sectionOne}>
+          <h1>Who we are</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+
+        <div className={styles.aboutUsContainer_sectionTwo}>
+          <h1>Who we are</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
     </Layout>
   )
 }
