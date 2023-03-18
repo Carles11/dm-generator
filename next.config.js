b497377d92af97ae1ef7e4ef6a60967bd63c0357
carlos <carlos@3pace.dev>
2023-03-18 09:51:28 -0300

    runned codemod for image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      reactStrictMode: true,
+  reactStrictMode: true,
+  // !! WARN !!
+  // Dangerously allow production builds to successfully complete even if
+  // your project has type errors.
+  // !! WARN !!
+  ignoreBuildErrors: false,
+  experimental: {
+    appDir: true, // https://nextjs.org/docs/messages/experimental-app-dir-config
+  },
 }
 
 module.exports = nextConfig
--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -21,7 +21,21 @@ export default function aboutUs() {
         </div>
 
         <div className={styles.aboutUsContainer_sectionTwo}>
-          <h1>Who we are</h1>
+          <h1>Why we do this?</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+        <div className={styles.aboutUsContainer_sectionOne}>
+          <h1>How can we do this?</h1>
           <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
